2f05f421db1c5ae8e9d7339741aa61cc57db27f9
fabiomarzano <fabiomarzano1993@gmail.com>
2024-08-06 22:14:29 +0200

    [UPD] structur-html

--- vite-yu-gi-oh/src/App.vue
+++ vite-yu-gi-oh/src/App.vue
@@ -1,30 +1,56 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import AppHeader from './components/header/AppHeader.vue';
+import AppMain from './components/main/AppMain.vue';
+import { store } from './store';
+import axios from 'axios';
+
+
+export default {
+    components: {
+        AppHeader,
+        AppMain,
+    },
+
+    data() {
+        return {
+            apiUrl: `https://db.ygoprodeck.com/api/v7/cardinfo.php`,
+            store,
+        }
+    },
+
+    methods: {
+        getCardInfo() {
+            axios.get(this.apiUrl, {
+                params: {
+                    num: this.store.numOfCards,
+                    offset: this.store.offsetNum,
+                    archetype: this.store.activeArchetype,
+                }
+            })
+                .then(response => {
+                    this.store.cardsList = response.data.data;
+                    console.log(response.data.data);
+                })
+
+                .catch(err => {
+                    console.warn(err.message);
+                })
+        }
+    },
+
+    created() {
+        this.getCardInfo();
+    }
+}
 </script>
 
+
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <AppHeader />
+    <AppMain @cardArchetypeSearch="getCardInfo" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
+<style lang="scss">
+@use './style/general.scss' as *;
 </style>
