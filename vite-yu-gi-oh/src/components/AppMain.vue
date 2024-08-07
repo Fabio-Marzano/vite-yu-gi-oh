68eca0b40bceee26271b18bc0e521cd43ad88f67
fabiomarzano <fabiomarzano1993@gmail.com>
2024-08-07 19:50:12 +0200

    [UPD] create-app-main-components

---
+++ vite-yu-gi-oh/src/components/AppMain.vue
@@ -0,0 +1,46 @@
+<script>
+import { store } from './../../store';
+import CardsArchetypeSelectField from './CardsArchetypeSelectField.vue';
+import CardsContainer from './CardsContainer.vue';
+
+
+export default {
+    name: 'AppMain',
+
+    components: {
+        CardsArchetypeSelectField,
+        CardsContainer,
+    },
+
+    data() {
+        return {
+            store,
+        }
+    },
+}
+</script>
+
+
+<template>
+    <main @change="$emit('cardArchetypeSearch')">
+        <div class="container" :class="(store.cardsList.length !== store.numOfCards) ? 'loader-container' : ''">
+            <CardsArchetypeSelectField />
+            <CardsContainer />
+        </div>
+    </main>
+</template>
+
+
+<style lang="scss" scoped>
+@use '../../style/partials/variables' as *;
+
+
+
+main {
+    background-color: $primary--bg-color;
+
+    >.container {
+        padding: 1.7rem 8.2rem;
+    }
+}
+</style>
