02aed525836779f1f89e1cb1faca8738d3e6cf4d
fabiomarzano <fabiomarzano1993@gmail.com>
2024-08-07 19:52:02 +0200

    [UPD] create-cards-archetype-select-file

---
+++ vite-yu-gi-oh/src/components/CardsArchetyipeSelect.vue
@@ -0,0 +1,32 @@
+<script>
+import { store } from './../../store';
+
+
+
+export default {
+    name: 'CardsArchetypeSelectField',
+
+    data() {
+        return {
+            store,
+        }
+    },
+}
+</script>
+
+
+<template>
+    <select name="cards-archetype" id="cards-archetype" v-model="store.activeArchetype">
+        <option :value="undefined">None</option>
+        <option v-for="archetype in store.archetypes" :value="archetype">{{ archetype }}</option>
+    </select>
+</template>
+
+
+<style lang="scss">
+select {
+    padding: 0.5rem;
+    margin-bottom: 1.7rem;
+    cursor: pointer;
+}
+</style>
